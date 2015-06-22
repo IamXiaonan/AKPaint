6f94b892b3e87123873b584c8aa633274d2f3a2f
MissWan <alinawxn@gmail.com>
2015-06-22 15:08:37 -0400

    Initial Commit

---
+++ AKPaint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AKPaint
+//
+//  Created by Xiaonan Wang on 6/22/15.
+//  Copyright (c) 2015 Xiaonan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
